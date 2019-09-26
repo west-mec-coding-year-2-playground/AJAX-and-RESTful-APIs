1895ddf3bc4b5fa2f7ad7601c87b495139d4cfc8
Trent <tpeterson998@west-mec.org>
2019-09-26 14:15:03 -0700

    Fixing stuff

--- Projects/Project_Lab/www/StockCheck.php
+++ Projects/Project_Lab/www/StockCheck.php
@@ -1,10 +1,13 @@
 <?php
-$TickerSymbol = $_GET["t"];
-header("Cache-Control: no-cache");
-// header("Content-Type: text/csv");
-// $Quote = "http://quote.yahoo.com/d/quotes.csv?s=$TickerSymbol&f=sl1d1t1c1p2ohgv";
-header("Content-Type: application/json");
- $Quote = "https://api.iextrading.com/1.0/stock/TickerSymbol/quote";
-$QuoteString = file_get_contents($Quote);
-echo $QuoteString;
+    $TickerSymbol = $_GET["t"];
+    $DateOne = $_GET["s"];
+    $DateTwo = $_GET["e"];
+
+    header("Cache-Control: no-cache");
+    header("Content-Type: text/csv");
+
+    $Quote = "https://www.quandl.com/api/v3/datasets/WIKI/$TickerSymbol.json?end_date=$DateTwo&start_date=$DateOne";
+    $QuoteString = file_get_contents($Quote);
+
+    echo $QuoteString;
 ?>
--- Projects/Project_Lab/www/script.js
+++ Projects/Project_Lab/www/script.js
@@ -18,10 +18,12 @@ function getRequestObject() {
     } catch (requestError) {
         return false;
     }
+
     return httpRequest;
 }
 
 function stopSubmission(evt) {
+
     if (evt.preventDefault) {
         evt.preventDefault();
     } else {
@@ -31,34 +33,19 @@ function stopSubmission(evt) {
 }
 
 function getQuote() {
+
     if (document.getElementsByTagName("input")[0].value) {
         entry = document.getElementsByTagName("input")[0].value;
-    } else {
-        document.getElementsByTagName("input")[0].value = entry;
+    }
+    if (!httpRequest) {
+        httpRequest = getRequestObject();
     }
 }
-
-function displayData() {
-    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
-        var stockResults = httpRequest.responseText;
-        console.log(stockResults);
-    }
-}
-
-function formatTable() {
-    var rows = document.getElementsByTagName("tr");
-    for (var i = 0; i < rows.length; i++) {
-        rows[i].style.background = "#9FE098";
-    }
-}
-
 var form = document.getElementsByTagName("form")[0];
 if (form.addEventListener) {
     form.addEventListener("submit", stopSubmission, false);
-    window.addEventListener("load", formatTable, false);
     window.addEventListener("load", getQuote, false);
 } else if (form.attachEvent) {
     form.attachEvent("onsubmit", stopSubmission);
-    window.attachEvent("onload", formatTable);
     window.attachEvent("onload", getQuote);
 }
