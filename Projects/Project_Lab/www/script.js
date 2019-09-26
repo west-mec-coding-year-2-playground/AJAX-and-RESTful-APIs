3157cdc7605057dae49b813a33a64f69057d0981
Trent <tpeterson998@west-mec.org>
2019-09-26 14:26:32 -0700

    finished the project

--- Projects/Project_Lab/www/script.js
+++ Projects/Project_Lab/www/script.js
@@ -33,19 +33,68 @@ function stopSubmission(evt) {
 }
 
 function getQuote() {
-
+    let current = new Date();
+    let threeDaysAgo = new Date();
+    threeDaysAgo.setDate(current.getDate() - 3);
     if (document.getElementsByTagName("input")[0].value) {
         entry = document.getElementsByTagName("input")[0].value;
     }
     if (!httpRequest) {
         httpRequest = getRequestObject();
     }
+    let dateOne = `2017-${getMonth(threeDaysAgo.getMonth())}-${threeDaysAgo.getDate()}`;
+    let dateTwo = `2017-${getMonth(current.getMonth())}-${current.getDate()}`
+
+    httpRequest.abort();
+    httpRequest.open("get", `StockCheck.php?t=${entry}&s=${dateOne}&e=${dateTwo}`, true);
+    httpRequest.send(null);
+    httpRequest.onreadystatechange = displayData;
+}
+
+function displayData() {
+    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+        var stockResults = httpRequest.responseText;
+        var stockItems;
+        try {
+            stockItems = JSON.parse(stockResults);
+        } catch (error) {
+            document.getElementById("ticker").innerHTML = "Error: Invalid Ticker.";
+            document.getElementById("openingPrice").innerHTML = "-";
+            document.getElementById("lastTrade").innerHTML = "-";
+            document.getElementById("lastTradeDT").innerHTML = "-";
+            document.getElementById("change").innerHTML = "-";
+            document.getElementById("range").innerHTML = "-";
+            document.getElementById("volume").innerHTML = "-";
+            return;
+        }
+        document.getElementById("ticker").innerHTML = stockItems.dataset.dataset_code;
+        document.getElementById("openingPrice").innerHTML = stockItems.dataset.data[0][1];
+        document.getElementById("lastTrade").innerHTML = stockItems.dataset.data[1][4];
+        document.getElementById("lastTradeDT").innerHTML = stockItems.dataset.data[1][0].replace("2017", new Date().getFullYear());
+        document.getElementById("change").innerHTML = ((parseFloat(stockItems.dataset.data[1][4]) - parseFloat(stockItems.dataset.data[0][1]))).toFixed(2);
+        document.getElementById("range").innerHTML = "Low " + stockItems.dataset.data[0][3] + "<br>High " + stockItems.dataset.data[0][2];
+        document.getElementById("volume").innerHTML = stockItems.dataset.data[0][5];
+    }
+}
+
+function formatTable() {
+    var rows = document.getElementsByTagName("tr");
+    for (var i = 0; i < rows.length; i++) {
+        rows[i].style.background = "#9FE098";
+    }
+}
+
+function getMonth(month) {
+    var curMonth = month > 0 ? month + 1 : 12;
+    return curMonth < 10 ? "0" + curMonth : curMonth;
 }
 var form = document.getElementsByTagName("form")[0];
 if (form.addEventListener) {
     form.addEventListener("submit", stopSubmission, false);
+    window.addEventListener("load", formatTable, false);
     window.addEventListener("load", getQuote, false);
 } else if (form.attachEvent) {
     form.attachEvent("onsubmit", stopSubmission);
+    window.attachEvent("onload", formatTable);
     window.attachEvent("onload", getQuote);
 }
